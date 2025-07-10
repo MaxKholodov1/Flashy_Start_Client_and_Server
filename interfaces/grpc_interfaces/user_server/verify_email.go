91cc821e65e3b8925ae679b0df4b4746ec9e546f
MaxKholodov1 <maksholodov8@gmail.com>
2025-07-10 10:19:41 +0300

    fix error in verifying email

--- interfaces/grpc_interfaces/user_server/verify_email.go
+++ interfaces/grpc_interfaces/user_server/verify_email.go
@@ -2,27 +2,27 @@ package user_server
 
 import (
 	"context"
+	"go_server/application/use_cases"
 	"go_server/infrastructure/grpc_infr/user/user"
 )
 
 func (s *UserServiceServer) VerifyEmail(ctx context.Context, req *user.VerifyEmailRequest) (*user.VerifyEmailResponse, error) {
 	isSuccess, err := s.userUseCases.VerifyEmail(ctx, req.Code, int(req.UserId))
 	if err != nil {
+		return nil, MapUserErrToGrpcErr(err)
+	}
+	if !isSuccess {
 		return &user.VerifyEmailResponse{
-			IsSuccess: false,
-		}, err
+			IsSuccess: isSuccess,
+		}, nil
 	}
 	refreshToken, err := s.tokenService.GenerateRefreshToken(int(req.UserId))
 	if err != nil {
-		return &user.VerifyEmailResponse{
-			IsSuccess: false,
-		}, err
+		return nil, MapUserErrToGrpcErr(use_cases.ErrFailedToGenerateToken)
 	}
 	accessToken, err := s.tokenService.GenerateAccessToken(int(req.UserId))
 	if err != nil {
-		return &user.VerifyEmailResponse{
-			IsSuccess: false,
-		}, err
+		return nil, MapUserErrToGrpcErr(use_cases.ErrFailedToGenerateToken)
 	}
 
 	return &user.VerifyEmailResponse{
