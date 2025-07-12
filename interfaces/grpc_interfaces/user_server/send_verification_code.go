708b6d3ee330c6d88909eb4843328532a6c9246f
MaxKholodov1 <maksholodov8@gmail.com>
2025-07-12 17:27:41 +0300

    fix interface of grpc

--- interfaces/grpc_interfaces/user_server/send_verification_code.go
+++ interfaces/grpc_interfaces/user_server/send_verification_code.go
@@ -5,7 +5,7 @@ import (
 	"go_server/infrastructure/grpc_infr/user/user"
 )
 
-func (s *UserServiceServer) SentVerificationCode(ctx context.Context, req *user.SendVerificationCodeRequest) (*user.SendVerificationCodeResponse, error) {
+func (s *UserServiceServer) SendVerificationCode(ctx context.Context, req *user.SendVerificationCodeRequest) (*user.SendVerificationCodeResponse, error) {
 	isSuccess, err := s.userUseCases.SendVerificationCode(ctx, int(req.UserId))
 	if err != nil {
 		return nil, err
